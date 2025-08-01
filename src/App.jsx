601c630475787a995a35e8e70ef2faacb9d4d5aa
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-07-31 21:19:04 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,6 +7,10 @@ function App() {
 
   return (
     <>
+    <div className='card'>
+      <h2>Rick Sanchez</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    </div>
      
     </>
   )
