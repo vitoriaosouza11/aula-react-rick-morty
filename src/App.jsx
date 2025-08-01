037228644aea36b295e57b1fa386e189d75e54b1
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-07-31 21:52:57 -0300

    imagens

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,20 @@ function App() {
     foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
+  const item2 = {
+    nome: 'Morty Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    nome: 'Summer Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
   return (
     <>
-      <Card info={item} />
+      <Card info={item1} />
+      <Card info={item2} />
+      <Card info={item3} />
     </>
   )
 }
